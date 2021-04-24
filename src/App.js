2f07534151113ae02100ef65dd49ff1390250ca4
Felicie Amundaray <feliamunda@outlook.com>
2021-04-23 21:46:48 -0400

    🎉 Init Project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body>
+        <h1>Hola Mundo</h1>
+        <img src={logo} alt="React"/>
+      </body>
+    </div>
+  );
+}
+
+export default App;
